4b41fa6e98653f8ccea45e8e36fe94dd16c01574
Vanessa Nakahara <vsnakahara@gmail.com>
2019-11-25 07:21:26 -0300

    atualizada rota para listar candidato

--- src/app/listar-candidatos/visualizar-candidato/visualizar-candidato.component.ts
+++ src/app/listar-candidatos/visualizar-candidato/visualizar-candidato.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
+import { CandidatoService } from "../../services/candidato.service";
+import { Candidato } from "src/app/models/candidato";
 
 @Component({
   selector: "app-visualizar-candidato",
@@ -7,14 +9,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ["./visualizar-candidato.component.scss"]
 })
 export class VisualizarCandidatoComponent implements OnInit {
-  constructor(private activatedRoute: ActivatedRoute) {}
+  candidato: Candidato;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private candidatoService: CandidatoService
+  ) {}
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       let id = params["id"];
 
       console.log(`${id}`);
+      this.candidatoService
+        .getCandidato(id)
+        .subscribe(candidato => (this.candidato = candidato));
     });
   }
-  
 }
